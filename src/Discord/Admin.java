b249d97e1c9700387dacd782aa96eda5de5ba776
akyla <akylaisyi@yahoo.com>
2023-05-27 23:48:36 -0500

    added Admin Class

---
+++ src/Discord/Admin.java
@@ -0,0 +1,22 @@
+package Discord;
+
+public class Admin extends DiscordUser{
+
+    public Admin(String role, String name, int id) {
+        super(role, name, id);
+    }
+
+    @Override
+    public void joinDiscord(String DiscordURL) {
+        System.out.println("Sending announcement");
+    }
+
+    public static void main(String[] args) {
+        Admin admin=new Admin("QA Engineer","Akylai", 148569);
+        System.out.println(admin.createdDate);
+        admin.senMessage();
+        System.out.println(admin);
+
+    }
+
+}
