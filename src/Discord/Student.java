25203c97e6ee37379fabb3e7600d8ac5a710fc13
akyla <akylaisyi@yahoo.com>
2023-05-28 00:21:43 -0500

    added Student

---
+++ src/Discord/Student.java
@@ -0,0 +1,15 @@
+package Discord;
+
+
+public class Student extends DiscordUser{
+    public static void main(String[] args) {
+        Student student= new Student("Student","Aki", 2546);
+        student.joinDiscord("http://gjtghdufs");
+    }
+int loveforJava;
+    public Student(String role, String name, int id) {
+        super("Student", name, id);
+        this.loveforJava=loveforJava;
+
+    }
+}
