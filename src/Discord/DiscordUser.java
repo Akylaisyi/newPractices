865e8f131a58e6e25eefb2a903905e04be6913b5
akyla <akylaisyi@yahoo.com>
2023-05-27 23:33:29 -0500

    added new Discord class

---
+++ src/Discord/DiscordUser.java
@@ -0,0 +1,26 @@
+package Discord;
+
+import java.time.LocalDate;
+
+public class DiscordUser {
+    String role;
+    String name;
+    int id;
+    LocalDate createdDate;
+    public DiscordUser(String role, String name,int id){
+        this.role=role;
+        this.name=name;
+        this.id=id;
+        createdDate=LocalDate.now();
+    }
+    public void senMessage(){
+        System.out.println("Sending message to class chat");
+    }
+    public void joinDiscord(String DiscordURL){
+        System.out.println("Joining "+DiscordURL);
+    }
+    @Override
+    public String toString(){
+        return name+"#"+id+"\nRoles: "+role+"\nAccount Created: "+createdDate;
+    }
+}
