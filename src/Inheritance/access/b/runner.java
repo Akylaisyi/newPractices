f31e177e4a33d38771bbfdfe368ce638f454d80b
akyla <akylaisyi@yahoo.com>
2023-05-27 21:18:46 -0500

    added runner in package b

---
+++ src/Inheritance/access/b/runner.java
@@ -0,0 +1,12 @@
+package Inheritance.access.b;
+
+public class runner {
+    public static void main(String[] args) {
+        Mac mac=new Mac();
+        //System.out.println(mac.memory);
+        System.out.println(mac.OS);
+        System.out.println(mac.toString());
+    }
+}
+
+
