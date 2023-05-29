9bb639255f556c2189895939faeaf8e42ea27243
akyla <akylaisyi@yahoo.com>
2023-05-29 10:12:47 -0500

    added Running class

---
+++ src/Discord/Abstraction/Running.java
@@ -0,0 +1,14 @@
+package Discord.Abstraction;
+
+public class Running extends Exercise{
+    @Override
+    public void perform() {
+        System.out.println("Running for fun");
+
+    }
+
+    @Override
+    public int getCaloriesBurned(int minutes) {
+        return minutes*15;
+    }
+}
